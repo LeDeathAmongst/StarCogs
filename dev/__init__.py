e5c2b13fa257299537c94a8b94427f389f6cd47b
PBOwner <skylar.rae.val@proton.me>
2024-09-03 06:34:13 -0400

    Fixed the module

--- dev/__init__.py
+++ dev/__init__.py
@@ -6,7 +6,7 @@ try:
     import Star_Utils
 except ModuleNotFoundError:
     raise errors.CogLoadError(
-        "The needed utils to run the cog were not found. Please execute the command `[p]pipinstall git+https://github.com/LeDeathAmongst/Star-Utils.git`. A restart of the bot isn't necessary."
+        "The needed utils to run the cog were not found. Please execute the command `[p]pipinstall git+https://github.com/LeDeathAmongst/Star_Utils.git`. A restart of the bot isn't necessary."
     )
 modules = sorted(
     [module for module in sys.modules if module.split(".")[0] == "Star_Utils"], reverse=True
