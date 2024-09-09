53a6c238a0a8367cbb6012e0215c557d9ffee480
LeDeathAmongst <180091824+LeDeathAmongst@users.noreply.github.com>
2024-09-09 00:52:30 -0400

    Update voicemeister.py

--- voicemeister/voicemeister.py
+++ voicemeister/voicemeister.py
@@ -439,7 +439,7 @@ def check_perms_source_dest(
 
     return required_check, optional_check, details
 
-    @commands.group(name="voicemeisterset", aliases=["vmset"])
+    @commands.group()
     @commands.guild_only()
     @checks.admin_or_permissions(manage_guild=True)
     async def voicemeisterset(self, ctx: commands.Context) -> None:
