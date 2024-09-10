3fa4e9827cbc0745b38c1aabd2d3e765c21d1d15
LeDeathAmongst <180091824+LeDeathAmongst@users.noreply.github.com>
2024-09-10 19:53:14 -0400

    Update source file __init__.py

--- voicemeister/__init__.py
+++ voicemeister/__init__.py
@@ -1,29 +1,38 @@
-from Star_Utils import Cog
-from redbot.core import errors
 import importlib
 import sys
+from redbot.core import errors
+from redbot.core.bot import Red
+from redbot.core.utils import get_end_user_data_statement
+from .voicemeister import VoiceMeister
+#from .c_voicemeister import VoiceMeisterCommands
+#from .c_voicemeisterset import VoiceMeisterSet
+#from .vminterface import VMInterface
+
+# Ensure Star_Utils is available
 try:
     import Star_Utils
 except ModuleNotFoundError:
     raise errors.CogLoadError(
         "The needed utils to run the cog were not found. Please execute the command `[p]pipinstall git+https://github.com/LeDeathAmongst/Star_Utils.git`. A restart of the bot isn't necessary."
-        )
-modules = sorted([module for module in sys.modules if module.split('.')[0] ==
-    'Star_Utils'], reverse=True)
+    )
+
+# Reload Star_Utils modules
+modules = sorted([module for module in sys.modules if module.split('.')[0] == 'Star_Utils'], reverse=True)
 for module in modules:
     try:
         importlib.reload(sys.modules[module])
     except ModuleNotFoundError:
         pass
 del Star_Utils
-from redbot.core.bot import Red
-from redbot.core.utils import get_end_user_data_statement
-from .voicemeister import VoiceMeister
+
 __red_end_user_data_statement__ = get_end_user_data_statement(file=__file__)
 
-
-async def setup(bot: Red) ->None:
+async def setup(bot: Red) -> None:
+#    vm_interface = VMInterface(bot)
     voicemeister_cog = VoiceMeister(bot)
-    # voicemeisterset_cog = VoiceMeisterSetCommands(bot)
+#    voicemeister_commands_cog = VoiceMeisterCommands(bot, vm_interface)
+#    voicemeister_set_cog = VoiceMeisterSet(bot)
+
     await bot.add_cog(voicemeister_cog)
-    # await bot.add_cog(voicemeisterset_cog)
+#    await bot.add_cog(voicemeister_commands_cog)
+#    await bot.add_cog(voicemeister_set_cog)
