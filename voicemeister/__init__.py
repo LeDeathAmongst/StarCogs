1344642eca8555d62e511cffe6ae329cb3a20ed5
LeDeathAmongst <180091824+LeDeathAmongst@users.noreply.github.com>
2024-09-09 21:07:09 -0400

    Update source file __init__.py

--- voicemeister/__init__.py
+++ voicemeister/__init__.py
@@ -1,34 +1,29 @@
-from redbot.core import errors  # isort:skip
+from Star_Utils import Cog
+from redbot.core import errors
 import importlib
 import sys
-
 try:
-    import Star_Utils  # Attempt to import Star_Utils
+    import Star_Utils
 except ModuleNotFoundError:
     raise errors.CogLoadError(
         "The needed utils to run the cog were not found. Please execute the command `[p]pipinstall git+https://github.com/LeDeathAmongst/Star_Utils.git`. A restart of the bot isn't necessary."
-    )
-
-# Reload Star_Utils modules if they are already loaded
-modules = sorted(
-    [module for module in sys.modules if module.split(".")[0] == "Star_Utils"], reverse=True
-)
+        )
+modules = sorted([module for module in sys.modules if module.split('.')[0] ==
+    'Star_Utils'], reverse=True)
 for module in modules:
     try:
         importlib.reload(sys.modules[module])
     except ModuleNotFoundError:
         pass
 del Star_Utils
-
-# Import Red and get_end_user_data_statement
-from redbot.core.bot import Red  # isort:skip
+from redbot.core.bot import Red
 from redbot.core.utils import get_end_user_data_statement
-
-from .voicemeister import VoiceMeister 
-
+from .voicemeister import VoiceMeisterSetCommands, VoiceMeister
 __red_end_user_data_statement__ = get_end_user_data_statement(file=__file__)
 
-async def setup(bot: Red) -> None:
-    cog = VoiceMeister(bot)
 
-    await bot.add_cog(cog)
+async def setup(bot: Red) ->None:
+    voicemeister_cog = VoiceMeister(bot)
+    voicemeisterset_cog = VoiceMeisterSetCommands(bot)
+    await bot.add_cog(voicemeister_cog)
+    await bot.add_cog(voicemeisterset_cog)
