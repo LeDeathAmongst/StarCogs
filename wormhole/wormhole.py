7a8426d57207add6d940e0832d8e2078bdae4b7a
LeDeathAmongst <180091824+LeDeathAmongst@users.noreply.github.com>
2024-09-28 09:48:39 -0400

    Update wormhole.py

--- wormhole/wormhole.py
+++ wormhole/wormhole.py
@@ -1,30 +1,74 @@
 import discord
-import os
 from redbot.core import commands, Config
 from datetime import datetime, timedelta
 import re
-from Star_Utils import Cog
+from Star_Utils import Cog, Settings
 
 class WormHole(Cog):
     def __init__(self, bot):
+        super().__init__(bot)  # Initialize the parent class
         self.bot = bot
         self.config = Config.get_conf(self, identifier="wormhole", force_registration=True)
-        self.config.register_global(
-            linked_channels_list=[],
-            global_blacklist=[],
-            word_filters=[],
-            mention_bypass_users=[],
-            use_webhooks=True,
-            image_mode="user",
-            name_mode="user"
-        )
-        self.message_references = {}
-        self.relayed_messages = {}
-        self.user_ping_count = {}
-        self.recent_messages = {}
         self.webhook_cache = {}
 
-        self.bot.remove_command('command_to_remove')
+        # Initialize settings using Settings class
+        self.settings = Settings(
+            bot=self.bot,
+            cog=self,
+            config=self.config,
+            group="global",
+            settings={
+                "linked_channels_list": {
+                    "path": ["linked_channels_list"],
+                    "converter": list,
+                    "command_name": "linkedchannels",
+                    "label": "Linked Channels",
+                    "description": "List of linked channels.",
+                },
+                "use_webhooks": {
+                    "path": ["use_webhooks"],
+                    "converter": bool,
+                    "command_name": "webhooks",
+                    "label": "Use Webhooks",
+                    "description": "Enable or disable the use of webhooks.",
+                },
+                "image_mode": {
+                    "path": ["image_mode"],
+                    "converter": str,
+                    "command_name": "imagemode",
+                    "label": "Image Mode",
+                    "description": "Set the image mode for webhooks. Options: user, server",
+                },
+                "name_mode": {
+                    "path": ["name_mode"],
+                    "converter": str,
+                    "command_name": "namemode",
+                    "label": "Name Mode",
+                    "description": "Set the name mode for webhooks. Options: user, server, both",
+                },
+                "global_blacklist": {
+                    "path": ["global_blacklist"],
+                    "converter": list,
+                    "command_name": "globalblacklist",
+                    "label": "Global Blacklist",
+                    "description": "List of globally blacklisted users.",
+                },
+                "word_filters": {
+                    "path": ["word_filters"],
+                    "converter": list,
+                    "command_name": "wordfilters",
+                    "label": "Word Filters",
+                    "description": "List of filtered words.",
+                },
+                "mention_bypass_users": {
+                    "path": ["mention_bypass_users"],
+                    "converter": list,
+                    "command_name": "mentionbypass",
+                    "label": "Mention Bypass Users",
+                    "description": "List of users allowed to bypass mention filters.",
+                },
+            }
+        )
 
     async def get_webhook(self, channel):
         if channel.id in self.webhook_cache:
@@ -39,7 +83,7 @@ class WormHole(Cog):
         return webhook
 
     async def send_status_message(self, message, channel, title):
-        linked_channels = await self.config.linked_channels_list()
+        linked_channels = await self.settings.get_raw("linked_channels_list")
         guild = channel.guild
         embed = discord.Embed(title=title, description=f"{guild.name}: {message}")
         for channel_id in linked_channels:
@@ -65,10 +109,10 @@ class WormHole(Cog):
     @wormhole.command(name="open")
     async def wormhole_open(self, ctx):
         """Link the current channel to the wormhole network."""
-        linked_channels = await self.config.linked_channels_list()
+        linked_channels = await self.settings.get_raw("linked_channels_list")
         if ctx.channel.id not in linked_channels:
             linked_channels.append(ctx.channel.id)
-            await self.config.linked_channels_list.set(linked_channels)
+            await self.settings.set_raw("linked_channels_list", linked_channels)
             embed = discord.Embed(title="Success!", description="This channel has joined the ever-changing maelstrom that is the wormhole.")
             await ctx.send(embed=embed)
             await self.send_status_message(f"A faint signal was picked up from {ctx.channel.mention}, connection has been established.", ctx.channel, "Success!")
@@ -79,77 +123,15 @@ class WormHole(Cog):
     @wormhole.command(name="close")
     async def wormhole_close(self, ctx):
         """Unlink the current channel from the wormhole network."""
-        linked_channels = await self.config.linked_channels_list()
+        linked_channels = await self.settings.get_raw("linked_channels_list")
         if ctx.channel.id in linked_channels:
             linked_channels.remove(ctx.channel.id)
-            await self.config.linked_channels_list.set(linked_channels)
+            await self.settings.set_raw("linked_channels_list", linked_channels)
             embed = discord.Embed(title="Success!", description="This channel has been severed from the wormhole.")
             await ctx.send(embed=embed)
             await self.send_status_message(f"The signal from {ctx.channel.mention} has become too faint to be picked up, the connection was lost.", ctx.channel, "Success!")
         else:
             embed = discord.Embed(title="ErRoR 404", description="This channel is not part of the wormhole.")
-            await ctx.send(embed=embed)
-
-    @wormhole.command(name="ownerclose")
-    @commands.is_owner()
-    async def wormhole_ownerclose(self, ctx, channel_id: int):
-        """Forcibly close a connection to the wormhole (Bot Owner Only)."""
-        linked_channels = await self.config.linked_channels_list()
-        if channel_id in linked_channels:
-            linked_channels.remove(channel_id)
-            await self.config.linked_channels_list.set(linked_channels)
-            channel = self.bot.get_channel(channel_id)
-            if channel:
-                embed = discord.Embed(title="Success!", description=f"The channel {channel.mention} (ID: {channel_id}) has been forcibly severed from the wormhole.")
-                await ctx.send(embed=embed)
-                await self.send_status_message(f"The signal from {channel.mention} has been forcibly severed by the bot owner.", channel, "Success!")
-            else:
-                embed = discord.Embed(title="Success!", description=f"The channel ID {channel_id} has been forcibly severed from the wormhole.")
-                await ctx.send(embed=embed)
-        else:
-            embed = discord.Embed(title="ErRoR 404", description=f"The channel ID {channel_id} is not part of the wormhole.")
-            await ctx.send(embed=embed)
-
-    @wormhole.command(name="servers")
-    async def wormhole_servers(self, ctx):
-        """List all servers connected to the wormhole."""
-        linked_channels = await self.config.linked_channels_list()
-        if not linked_channels:
-            await ctx.send(embed=discord.Embed(title="Wormhole Servers", description="No channels are currently linked to the wormhole.", color=discord.Color.red()))
-            return
-
-        server_list = {}
-        for channel_id in linked_channels:
-            channel = self.bot.get_channel(channel_id)
-            if channel:
-                guild = channel.guild
-                if guild not in server_list:
-                    server_list[guild] = []
-                server_list[guild].append(channel)
-
-        if not server_list:
-            await ctx.send(embed=discord.Embed(title="Wormhole Servers", description="No servers are currently linked to the wormhole.", color=discord.Color.red()))
-            return
-
-        embed = discord.Embed(title="Connected Servers", color=discord.Color.blue())
-        description = ""
-        for guild, channels in server_list.items():
-            owner = guild.owner
-            for channel in channels:
-                description += (
-                    f"**{guild.name}**\n"
-                    f"Owner: {owner} (ID: {owner.id})\n"
-                    f"Server ID: {guild.id}\n"
-                    f"Channel: {channel.mention} (ID: {channel.id})\n\n"
-                )
-                if len(description) > 1800:
-                    embed.description = description
-                    await ctx.send(embed=embed)
-                    embed = discord.Embed(title="Connected Servers", color=discord.Color.blue())
-                    description = ""
-
-        if description:
-            embed.description = description
             await ctx.send(embed=embed)
 
     @commands.Cog.listener()
@@ -158,12 +140,16 @@ class WormHole(Cog):
             return
         if message.author.bot or not message.channel.permissions_for(message.guild.me).send_messages:
             return
+        if isinstance(message.channel, discord.TextChannel) and message.content.startswith(commands.when_mentioned(self.bot, message)[0]):
+            return  # Ignore bot commands
+        if message.content.startswith(tuple(await self.bot.get_prefix(message))):
+            return  # Ignore messages starting with the bot's prefix
 
-        linked_channels = await self.config.linked_channels_list()
+        linked_channels = await self.settings.get_raw("linked_channels_list")
 
         if message.channel.id in linked_channels:
-            global_blacklist = await self.config.global_blacklist()
-            word_filters = await self.config.word_filters()
+            global_blacklist = await self.settings.get_raw("global_blacklist")
+            word_filters = await self.settings.get_raw("word_filters")
 
             if message.author.id in global_blacklist:
                 return
@@ -216,9 +202,9 @@ class WormHole(Cog):
 
             content = self.replace_emojis_with_urls(message.guild, content)
 
-            use_webhooks = await self.config.use_webhooks()
-            image_mode = await self.config.image_mode()
-            name_mode = await self.config.name_mode()
+            use_webhooks = await self.settings.get_raw("use_webhooks")
+            image_mode = await self.settings.get_raw("image_mode")
+            name_mode = await self.settings.get_raw("name_mode")
 
             files = [await attachment.to_file() for attachment in message.attachments]
 
@@ -270,7 +256,7 @@ class WormHole(Cog):
         if not after.guild:
             return
 
-        linked_channels = await self.config.linked_channels_list()
+        linked_channels = await self.settings.get_raw("linked_channels_list")
 
         if after.channel.id in linked_channels:
             display_name = after.author.display_name if after.author.display_name else after.author.name
@@ -285,7 +271,7 @@ class WormHole(Cog):
             for role in mentioned_roles:
                 content = content.replace(f"<@&{role.id}>", '')
 
-            if any(word in content for word in await self.config.word_filters()):
+            if any(word in content for word in await self.settings.get_raw("word_filters")):
                 embed = discord.Embed(title="ErRoR 404", description="That word is not allowed.")
                 await after.channel.send(embed=embed)
                 await after.delete()
@@ -313,7 +299,7 @@ class WormHole(Cog):
         if not message.guild:
             return
 
-        linked_channels = await self.config.linked_channels_list()
+        linked_channels = await self.settings.get_raw("linked_channels_list")
 
         if message.channel.id in linked_channels:
             for channel_id in linked_channels:
@@ -332,7 +318,7 @@ class WormHole(Cog):
 
     @commands.Cog.listener()
     async def on_member_ban(self, guild: discord.Guild, user: discord.User):
-        linked_channels = await self.config.linked_channels_list()
+        linked_channels = await self.settings.get_raw("linked_channels_list")
         for channel_id in linked_channels:
             channel = self.bot.get_channel(channel_id)
             if channel:
@@ -350,104 +336,3 @@ class WormHole(Cog):
         now = datetime.utcnow()
         cutoff = now - timedelta(hours=24)
         self.recent_messages = {msg_id: data for msg_id, data in self.recent_messages.items() if data['timestamp'] > cutoff}
-
-    @wormhole.command(name="globalblacklist")
-    async def wormhole_globalblacklist(self, ctx, user: discord.User):
-        """Prevent specific members from sending messages through the wormhole globally."""
-        if await self.bot.is_owner(ctx.author):
-            global_blacklist = await self.config.global_blacklist()
-            if user.id not in global_blacklist:
-                global_blacklist.append(user.id)
-                await self.config.global_blacklist.set(global_blacklist)
-                embed = discord.Embed(title="Success!", description=f"{user.display_name} has been added to the global wormhole blacklist.")
-                await ctx.send(embed=embed)
-            else:
-                embed = discord.Embed(title="ErRoR 404", description=f"{user.display_name} is already in the global wormhole blacklist.")
-                await ctx.send(embed=embed)
-        else:
-            embed = discord.Embed(title="ErRoR 404", description="You must be the bot owner to use this command.")
-            await ctx.send(embed=embed)
-
-    @wormhole.command(name="unglobalblacklist")
-    async def wormhole_unglobalblacklist(self, ctx, user: discord.User):
-        """Command to remove a user from the global wormhole blacklist (Bot Owner Only)."""
-        if await self.bot.is_owner(ctx.author):
-            global_blacklist = await self.config.global_blacklist()
-            if user.id in global_blacklist:
-                global_blacklist.remove(user.id)
-                await self.config.global_blacklist.set(global_blacklist)
-                embed = discord.Embed(title="Success!", description=f"{user.display_name} has been removed from the global wormhole blacklist.")
-                await ctx.send(embed=embed)
-            else:
-                embed = discord.Embed(title="ErRoR 404", description=f"{user.display_name} is not in the global wormhole blacklist.")
-                await ctx.send(embed=embed)
-        else:
-            embed = discord.Embed(title="ErRoR 404", description="You must be the bot owner to use this command.")
-            await ctx.send(embed=embed)
-
-    @wormhole.command(name="addwordfilter")
-    async def wormhole_addwordfilter(self, ctx, *, word: str):
-        """Add a word to the wormhole word filter."""
-        if await self.bot.is_owner(ctx.author):
-            word_filters = await self.config.word_filters()
-            if word not in word_filters:
-                word_filters.append(word)
-                await self.config.word_filters.set(word_filters)
-                embed = discord.Embed(title="Success!", description=f"`{word}` has been added to the wormhole word filter.")
-                await ctx.send(embed=embed)
-            else:
-                embed = discord.Embed(title="ErRoR 404", description=f"`{word}` is already in the wormhole word filter.")
-                await ctx.send(embed=embed)
-
-    @wormhole.command(name="removewordfilter")
-    async def wormhole_removewordfilter(self, ctx, *, word: str):
-        """Remove a word from the wormhole word filter."""
-        if await self.bot.is_owner(ctx.author):
-            word_filters = await self.config.word_filters()
-            if word in word_filters:
-                word_filters.remove(word)
-                await self.config.word_filters.set(word_filters)
-                embed = discord.Embed(title="Success!", description=f"`{word}` has been removed from the wormhole word filter.")
-                await ctx.send(embed=embed)
-            else:
-                embed = discord.Embed(title="ErRoR 404", description=f"`{word}` is not in the wormhole word filter.")
-                await ctx.send(embed=embed)
-
-    @wormhole.command(name="addmentionbypass")
-    @commands.is_owner()
-    async def wormhole_addmentionbypass(self, ctx, user: discord.User):
-        """Allow a user to bypass the mention filter."""
-        mention_bypass_users = await self.config.mention_bypass_users()
-        if user.id not in mention_bypass_users:
-            mention_bypass_users.append(user.id)
-            await self.config.mention_bypass_users.set(mention_bypass_users)
-            embed = discord.Embed(title="Success!", description=f"{user.display_name} has been allowed to bypass the mention filter.")
-            await ctx.send(embed=embed)
-        else:
-            embed = discord.Embed(title="ErRoR 404", description=f"{user.display_name} is already allowed to bypass the mention filter.")
-            await ctx.send(embed=embed)
-
-    @wormhole.command(name="removementionbypass")
-    @commands.is_owner()
-    async def wormhole_removementionbypass(self, ctx, user: discord.User):
-        """Remove a user's bypass for the mention filter."""
-        mention_bypass_users = await self.config.mention_bypass_users()
-        if user.id in mention_bypass_users:
-            mention_bypass_users.remove(user.id)
-            await self.config.mention_bypass_users.set(mention_bypass_users)
-            embed = discord.Embed(title="Success!", description=f"{user.display_name} is no longer allowed to bypass the mention filter.")
-            await ctx.send(embed=embed)
-        else:
-            embed = discord.Embed(title="ErRoR 404", description=f"{user.display_name} is not allowed to bypass the mention filter.")
-            await ctx.send(embed=embed)
-
-    @commands.Cog.listener()
-    async def on_typing(self, channel, user, when):
-        """Notify linked channels when a user is typing."""
-        linked_channels = await self.config.linked_channels_list()
-        if channel.id in linked_channels:
-            for channel_id in linked_channels:
-                if channel_id != channel.id:
-                    relay_channel = self.bot.get_channel(channel_id)
-                    if relay_channel:
-                        await relay_channel.trigger_typing()
