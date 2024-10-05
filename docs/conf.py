3ddf92b42cf468154114e619fa564d81f08f0a8b
Star Val <skylar.rae.val@proton.me>
2024-10-05 01:15:04 -0400

    Temp Change Theme (hopefully)

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ exclude_patterns = ["Thumbs.db", ".DS_Store", ".venv", "venv"]
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = "renku"
+html_theme = "sphinx_rtd_theme"
 html_css_files = ["literals.css"]
 
 # GitHub integration
