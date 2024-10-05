3b9ed99edadb1482b194738a19d09d34d274bf9a
LeDeathAmongst <180091824+LeDeathAmongst@users.noreply.github.com>
2024-10-05 10:00:54 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ exclude_patterns = ["Thumbs.db", ".DS_Store", ".venv", "venv"]
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = "topos-theme"
+html_title = "Starfire Docs"
 
 # GitHub integration
 html_context = {
@@ -21,14 +22,21 @@ html_context = {
     "github_user": "LeDeathAmongst",
     "github_repo": "StarCogs",
     "github_version": "main",
-    "style": "literal.css",
-    "site_name": 'Starfire Docs',
-    "bot_name": 'Starfire',
-    "prefix": ',',
-    "invite_link": 'https://discord.com/oauth2/authorize?client_id=1275521742961508432',
-    "support_server": 'https://discord.gg/HXdan6NnfJ',
+    "style": "literal.css"
+#    "site_name": 'Starfire Docs',
+#    "bot_name": 'Starfire',
+#    "prefix": ',',
+#    "invite_link": 'https://discord.com/oauth2/authorize?client_id=1275521742961508432',
+#    "support_server": 'https://discord.gg/HXdan6NnfJ',
 }
 
+rst_epilog = """
+.. |site_name| replace:: Starfire Docs
+.. |bot_name| replace:: Starfire
+.. |prefix| replace:: ,
+.. |invite_link| replace:: https://discord.com/oauth2/authorize?client_id=1275521742961508432
+.. |support_server| replace:: https://discord.gg/HXdan6NnfJ
+"""
 # Source file suffix
 source_suffix = ".rst"
 
