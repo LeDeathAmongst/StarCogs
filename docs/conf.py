75b71695a54a4829f2b81cbd4ab62317ca653968
LeDeathAmongst <180091824+LeDeathAmongst@users.noreply.github.com>
2024-10-05 09:04:29 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,6 @@ exclude_patterns = ["Thumbs.db", ".DS_Store", ".venv", "venv"]
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = "renku"
-html_css_files = ["literals.css"]
 
 # GitHub integration
 html_context = {
