012276d22b5646adae8bbdee1f995f451b934958
LeDeathAmongst <180091824+LeDeathAmongst@users.noreply.github.com>
2024-10-11 05:17:02 -0400

    Update __init__.py

--- gbl/__init__.py
+++ gbl/__init__.py
@@ -1,39 +1,45 @@
-from Star_Utils import Cog
 from redbot.core import errors
 import importlib
 import sys
+
 try:
     import Star_Utils
 except ModuleNotFoundError:
     raise errors.CogLoadError(
         "The needed utils to run the cog were not found. Please execute the command `[p]pipinstall git+https://github.com/LeDeathAmongst/Star_Utils.git`. A restart of the bot isn't necessary."
-        )
-modules = sorted([module for module in sys.modules if module.split('.')[0] ==
-    'Star_Utils'], reverse=True)
+    )
+
+modules = sorted([module for module in sys.modules if module.split('.')[0] == 'Star_Utils'], reverse=True)
 for module in modules:
     try:
         importlib.reload(sys.modules[module])
     except ModuleNotFoundError:
         pass
-del Star_Utils
+
 from redbot.core.bot import Red
 from redbot.core.utils import get_end_user_data_statement
 from .gbl import GlobalBanList
+
 __red_end_user_data_statement__ = get_end_user_data_statement(file=__file__)
 
-
-async def setup(bot):
+async def setup(bot: Red):
     cog = GlobalBanList(bot)
     await bot.add_cog(cog)
 
     # Set up autocomplete for app commands
-    add_user_command = cog.gbl.get_command("add")
-    remove_user_command = cog.gbl.get_command("remove")
-    subscribe_command = cog.gbl.get_command("subscribe")
-    unsubscribe_command = cog.gbl.get_command("unsubscribe")
+    gbl_group = cog.gbl
+    if gbl_group:
+        add_user_command = gbl_group.get_command("add")
+        remove_user_command = gbl_group.get_command("remove")
+        subscribe_command = gbl_group.get_command("subscribe")
+        unsubscribe_command = gbl_group.get_command("unsubscribe")
 
-    add_user_command.autocomplete("list_name")(cog.autocomplete_list_name)
-    remove_user_command.autocomplete("list_name")(cog.autocomplete_list_name)
-    remove_user_command.autocomplete("user")(cog.autocomplete_banned_user)
-    subscribe_command.autocomplete("list_name")(cog.autocomplete_list_name)
-    unsubscribe_command.autocomplete("list_name")(cog.autocomplete_list_name)
+        if add_user_command:
+            add_user_command.autocomplete("list_name")(cog.autocomplete_list_name)
+        if remove_user_command:
+            remove_user_command.autocomplete("list_name")(cog.autocomplete_list_name)
+            remove_user_command.autocomplete("user")(cog.autocomplete_banned_user)
+        if subscribe_command:
+            subscribe_command.autocomplete("list_name")(cog.autocomplete_list_name)
+        if unsubscribe_command:
+            unsubscribe_command.autocomplete("list_name")(cog.autocomplete_list_name)
