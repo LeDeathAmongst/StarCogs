713fc7d88ac5e24129260f4943eeb0f56949e1b8
LeDeathAmongst <180091824+LeDeathAmongst@users.noreply.github.com>
2024-10-11 02:46:16 -0400

    Update __init__.py

--- staff/__init__.py
+++ staff/__init__.py
@@ -18,10 +18,10 @@ for module in modules:
 del Star_Utils
 from redbot.core.bot import Red
 from redbot.core.utils import get_end_user_data_statement
-from .staff import StaffFlow
+from .staff import Staffer
 __red_end_user_data_statement__ = get_end_user_data_statement(file=__file__)
 
 
 async def setup(bot: Red) ->None:
-    cog = StaffFlow(bot)
+    cog = Staffer(bot)
     await bot.add_cog(cog)
