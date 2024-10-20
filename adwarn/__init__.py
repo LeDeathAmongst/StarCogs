40926d37c4fedb126a750a792987eaefee111931
LeDeathAmongst <180091824+LeDeathAmongst@users.noreply.github.com>
2024-10-19 23:14:15 -0400

    Update __init__.py

--- adwarn/__init__.py
+++ adwarn/__init__.py
@@ -18,10 +18,10 @@ for module in modules:
 del Star_Utils
 from redbot.core.bot import Red
 from redbot.core.utils import get_end_user_data_statement
-from .adwarn import AdWarn
+from .adwarn import Adwarn
 __red_end_user_data_statement__ = get_end_user_data_statement(file=__file__)
 
 
 async def setup(bot: Red) ->None:
-    cog = AdWarn(bot)
+    cog = Adwarn(bot)
     await bot.add_cog(cog)
