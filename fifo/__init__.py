2967e9404fa7ac91bd0af0dec8a7e1ebcebdc54c
LeDeathAmongst <180091824+LeDeathAmongst@users.noreply.github.com>
2024-10-20 01:09:15 -0400

    Update __init__.py

--- fifo/__init__.py
+++ fifo/__init__.py
@@ -1,5 +1,26 @@
 import sys
+import importlib
 
+from redbot.core import errors
+from redbot.core.bot import Red
+from redbot.core.utils import get_end_user_data_statement
+
+try:
+    import Star_Utils
+except ModuleNotFoundError:
+    raise errors.CogLoadError(
+        "The needed utils to run the cog were not found. Please execute the command `[p]pipinstall git+https://github.com/LeDeathAmongst/Star_Utils.git`. A restart of the bot isn't necessary."
+    )
+
+modules = sorted([module for module in sys.modules if module.split('.')[0] == 'Star_Utils'], reverse=True)
+for module in modules:
+    try:
+        importlib.reload(sys.modules[module])
+    except ModuleNotFoundError:
+        pass
+del Star_Utils
+
+from Star_Utils import Cog
 from .fifo import FIFO
 
 # Applying fix from: https://github.com/Azure/azure-functions-python-worker/issues/640
@@ -10,12 +31,11 @@ from . import CustomDateTrigger
 # [Fix] Register imgres into system modules
 sys.modules["CustomDateTrigger"] = CustomDateTrigger
 
+__red_end_user_data_statement__ = get_end_user_data_statement(file=__file__)
 
-async def setup(bot):
-    cog = FIFO(bot)
-    bot.add_cog(cog)
-    await cog.initialize()
+async def setup(bot: Red) -> None:
+    fifo_cog = FIFO(bot)
+    await bot.add_cog(fifo_cog)
 
-
-async def teardown(bot):
+async def teardown(bot: Red) -> None:
     pass
