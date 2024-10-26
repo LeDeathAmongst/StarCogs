6b91b498181842d1878c6ba54bfd114b0b087d0c
LeDeathAmongst <180091824+LeDeathAmongst@users.noreply.github.com>
2024-10-26 00:26:32 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ copyright = "2023 - Present | Star"
 # -- General configuration ---------------------------------------------------
 
 extensions = ["myst_parser","sphinx_rtd_theme"]
-templates_path = ["_templates"]
+# templates_path = ["_templates"]
 exclude_patterns = ["Thumbs.db", ".DS_Store", ".venv", "venv"]
 
 # -- Options for HTML output -------------------------------------------------
@@ -31,30 +31,9 @@ html_theme_options = {
         {"name": "Home", "url": "index"},
         {"name": "Features", "url": "features"},
         {"name": "Support", "url": "support"},
-    ],
-    "display_github": True,
-    "github_user": "LeDeathAmongst",
-    "github_repo": "StarCogs",
-    "github_version": "master",
-    "description": "Starfire Documentations site"
+    ]
 }
 
-html_context = {
-    "style": ""
-#    "site_name": 'Starfire Docs',
-#    "bot_name": 'Starfire',
-#    "prefix": ',',
-#    "invite_link": 'https://discord.com/oauth2/authorize?client_id=1275521742961508432',
-#    "support_server": 'https://discord.gg/HXdan6NnfJ',
-}
-
-rst_epilog = """
-.. |site_name| replace:: Starfire Docs
-.. |bot_name| replace:: Starfire
-.. |prefix| replace:: ,
-.. |invite_link| replace:: https://discord.com/oauth2/authorize?client_id=1275521742961508432
-.. |support_server| replace:: https://discord.gg/HXdan6NnfJ
-"""
 # Source file suffix
 source_suffix = ".md"
 
