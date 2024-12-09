413f74bc735d9a3b49e76e4b4f571081f094713c
Star Val <skylar.rae.val@proton.me>
2024-12-09 19:35:28 +0000

    Fixed

---
+++ lockunlock/__init__.py
@@ -0,0 +1,27 @@
+from redbot.core import errors
+import importlib
+import sys
+
+try:
+    import Star_Utils
+except ModuleNotFoundError:
+    raise errors.CogLoadError(
+        "The needed utils to run the cog were not found. Please execute the command `[p]pipinstall git+https://github.com/LeDeathAmongst/Star_Utils.git`. A restart of the bot isn't necessary."
+    )
+
+modules = sorted([module for module in sys.modules if module.split('.')[0] == 'Star_Utils'], reverse=True)
+for module in modules:
+    try:
+        importlib.reload(sys.modules[module])
+    except ModuleNotFoundError:
+        pass
+
+from redbot.core.bot import Red
+from redbot.core.utils import get_end_user_data_statement
+from .lockunlock import LockUnlock
+
+__red_end_user_data_statement__ = get_end_user_data_statement(file=__file__)
+
+async def setup(bot: Red):
+    cog = LockUnlock(bot)
+    await bot.add_cog(cog)
